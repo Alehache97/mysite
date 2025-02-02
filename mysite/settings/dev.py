c3cebaa5087ddec76aff77351a223c0a13c6e1a6
Alehache97 <alejandroherrera140697@gmail.com>
2025-02-02 22:22:53 +0000

    Primer commit

---
+++ mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-4_bqbkhj-3c83ffs@5(^&bnf7)c=9pj!r38hv8tz)mf^na8x*z"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
